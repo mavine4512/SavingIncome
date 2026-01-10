7d0de81a93d263798f1e61df072eeff73ac540fc
Mavine Naaman <oyugi.mavine@eclectics.io>
2026-01-10 17:25:34 +0300

    account ui fixed

--- src/app/accounts.tsx
+++ src/app/accounts.tsx
@@ -1,10 +1,28 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import AccountsList from '../components/AccountsList';
 
 export default function AccountsScreen(){
 
     return (
-        <View>
-            <Text>Accounts Screen</Text>
+        <View style={{gap:5, padding:5}}>
+            <View style={styles.header}>
+                <Text  style={styles.name}>Name</Text>
+                <Text style={styles.name}>CAP</Text>
+                <Text style={styles.name}>TAP</Text>
+            </View>
+            <AccountsList/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+   header:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    padding:10,
+   },
+   name:{
+    fontWeight:'bold',
+    fontSize:15
+   },
+})
---
+++ src/components/AccountsList.tsx
@@ -0,0 +1,14 @@
+import { FlatList } from 'react-native';
+import AccountListItem from './AccountListItem';
+
+
+export default function AccountsList(){
+    console.log('test')
+    return(
+           <FlatList
+           data={[1,2,3]}
+           contentContainerStyle={{gap:5}}
+           renderItem={()=><AccountListItem/>}
+           />
+    )
+}
